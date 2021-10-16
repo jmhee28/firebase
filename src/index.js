5dd969693fcfc54bbe0cceaa24ed76164ac5d389
Daniel Y. Lee <taeold@gmail.com>
2021-10-15 20:30:19 -0700

    Complete implementation of hw1.

--- src/index.js
+++ src/index.js
@@ -4,41 +4,67 @@ import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
 } from 'firebase/auth';
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp
+} from 'firebase/firestore'
 import { getFirebaseConfig } from './firebase-config.js';
 
-function signIn() {
-  // TODO: Complete this function.
+async function signIn() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  await signInWithPopup(getAuth(), new GoogleAuthProvider());
 }
 
-function signOut() {
-  // TODO: Complete this function.
+function signOutUser() {
+  signOut(getAuth());
 }
 
 // Saves a new message to the Cloud Firestore.
-function saveMessage(messageText) {
-  // TODO: Complete this function.
+async function saveMessage(messageText) {
+  try {
+    await addDoc(collection(getFirestore(), 'messages'), {
+      name: getUserName(),
+      text: messageText,
+      profilePicUrl: getProfilePicUrl(),
+      timestamp: serverTimestamp()
+    });
+  }
+  catch(error) {
+    console.error('Error writing new message to Firebase Database', error);
+  }
 }
 
 // Returns true if a user is signed-in.
 function isUserSignedIn() {
-  // TODO: Complete this function.
+  return !!getAuth().currentUser;
 }
 
 // Loads chat messages history and listens for upcoming ones.
 function loadMessages() {
-  // TODO: Complete this function.
-  // Create the query to load the last 12 messages and listen for new ones.
-  // var query = undefined;
+  const recentMessagesQuery = query(collection(getFirestore(), 'messages'), orderBy('timestamp', 'desc'), limit(12));
 
   // Start listening to the query.
-  // onSnapshot(query, function(snapshot) {
-  //   snapshot.docChanges().forEach(function(change) {
-  //     // TODO
-  //     // Call deleteMessage when message is removed.
-  //     // Call displayMessage when message is added.
-  //   });
-  // });
+  onSnapshot(recentMessagesQuery, function(snapshot) {
+    snapshot.docChanges().forEach(function(change) {
+      if (change.type === 'removed') {
+        deleteMessage(change.doc.id);
+      } else {
+        var message = change.doc.data();
+        displayMessage(change.doc.id, message.timestamp, message.name,
+                      message.text, message.profilePicUrl, message.imageUrl);
+      }
+    });
+  });
 }
 
 // Initiate firebase auth.
@@ -220,7 +246,7 @@ const noMessageElement = document.getElementById('no-message');
 
 // Saves message on form submit.
 messageFormElement.addEventListener('submit', onMessageFormSubmit);
-signOutButtonElement.addEventListener('click', signOut);
+signOutButtonElement.addEventListener('click', signOutUser);
 signInButtonElement.addEventListener('click', signIn);
 
 // Toggle for the button.
